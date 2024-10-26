31814339e909054163a7b138c65a6bce10e0d7e1
Adithya <138711762+Mirage042@users.noreply.github.com>
2024-10-27 04:43:54 +0530

    final

--- run.py
+++ run.py
@@ -2,14 +2,17 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-
 import os
+from flask import request, jsonify
 from flask_migrate import Migrate
 from flask_minify import Minify
 from sys import exit
-
+from apps import (
+    create_app,
+    db,
+    generate_response,
+)  # Import generate_response from __init__.py
 from apps.config import config_dict
-from apps import create_app, db
 
 # WARNING: Don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
@@ -18,10 +21,8 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 get_config_mode = "Debug" if DEBUG else "Production"
 
 try:
-
     # Load the configuration using the default values
     app_config = config_dict[get_config_mode.capitalize()]
-
 except KeyError:
     exit("Error: Invalid <config_mode>. Expected values [Debug, Production] ")
 
@@ -35,6 +36,15 @@ if DEBUG:
     app.logger.info("DEBUG       = " + str(DEBUG))
     app.logger.info("DBMS        = " + app_config.SQLALCHEMY_DATABASE_URI)
     app.logger.info("ASSETS_ROOT = " + app_config.ASSETS_ROOT)
+
+
+@app.route("/chatbot", methods=["POST"])
+def chatbot():
+    data = request.get_json()
+    user_message = data.get("message")
+    response = generate_response(user_message)  # Call the function from __init__.py
+    return jsonify({"response": response})
+
 
 if __name__ == "__main__":
     app.run()
